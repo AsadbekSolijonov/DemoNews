1d2947f9684156a2a0f82b0d6df7b2202ff553ce
AsadbekSolijonov <iamsolijonovasadbek@gmail.com>
2023-04-09 14:50:13 +0500

    bot loader

--- loader.py
+++ loader.py
@@ -1,8 +1,11 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-
+from utils.db_api.postgresql import PostgreSQLConnection
 from data import config
 
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
+# PostgreSQL Database
+db = PostgreSQLConnection()
+
