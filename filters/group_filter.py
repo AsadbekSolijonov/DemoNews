441adf7837670783f839f2a6ec4e1739f2f9817d
AsadbekSolijonov <iamsolijonovasadbek@gmail.com>
2023-04-09 14:48:42 +0500

    group filter

--- filters/group_filter.py
+++ filters/group_filter.py
@@ -1,6 +1,7 @@
 from aiogram.types import Message, ChatType
 from aiogram.dispatcher.filters import BoundFilter
 
+
 class GroupFilter(BoundFilter):
     async def check(self, message: Message):
         return message.chat.type in (ChatType.GROUP, ChatType.SUPER_GROUP)
