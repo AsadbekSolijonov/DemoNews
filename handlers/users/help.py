aecec15616e6e5756e0f995094fa92dac366f9e4
AsadbekSolijonov <iamsolijonovasadbek@gmail.com>
2023-04-09 14:49:49 +0500

    changed acho

--- handlers/users/help.py
+++ handlers/users/help.py
@@ -1,13 +1,15 @@
+from loader import dp
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandHelp
-
-from loader import dp
 
 
 @dp.message_handler(CommandHelp())
 async def bot_help(message: types.Message):
-    text = ("Buyruqlar: ",
-            "/start - Botni ishga tushirish",
-            "/help - Yordam")
-    
+    # Text
+    text = ("🤖 Commands: ",
+            "/start - Start the bot",
+            "/help - Help",
+            "/setting_weather_time - Setting the weather time",
+            "/current_weather - Current weather data")
+    # Message
     await message.answer("\n".join(text))
