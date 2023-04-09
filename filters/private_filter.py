63b91e8ea712d70be9417c71d271140c8a2cb397
AsadbekSolijonov <iamsolijonovasadbek@gmail.com>
2023-04-09 14:48:50 +0500

    private filter

--- filters/private_filter.py
+++ filters/private_filter.py
@@ -1,6 +1,7 @@
 from aiogram.types import Message, ChatType
 from aiogram.dispatcher.filters import BoundFilter
 
+
 class Private(BoundFilter):
     async def check(self, message: Message):
         return message.chat.type == ChatType.PRIVATE
