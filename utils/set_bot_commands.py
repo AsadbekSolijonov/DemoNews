22fb67630e68e6c5cad43add8ab2326f9cf90e3f
AsadbekSolijonov <iamsolijonovasadbek@gmail.com>
2023-04-09 14:50:44 +0500

    bot commands

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -4,7 +4,9 @@ from aiogram import types
 async def set_default_commands(dp):
     await dp.bot.set_my_commands(
         [
-            types.BotCommand("start", "Botni ishga tushurish"),
-            types.BotCommand("help", "Yordam"),
+            types.BotCommand("start", "Start the bot"),
+            types.BotCommand("help", "help"),
+            types.BotCommand("setting_weather_time", "Setting the weather time"),
+            types.BotCommand("current_weather", "Current weather data")
         ]
     )
