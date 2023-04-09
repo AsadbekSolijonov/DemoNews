691a3e79c17e22e9b154d0e34a5d56f903d0092c
AsadbekSolijonov <iamsolijonovasadbek@gmail.com>
2023-04-09 14:48:22 +0500

    private data

--- data/config.py
+++ data/config.py
@@ -1,10 +1,22 @@
 from environs import Env
 
-# environs kutubxonasidan foydalanish
+# using from environs
 env = Env()
 env.read_env()
 
-# .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-ADMINS = env.list("ADMINS")  # adminlar ro'yxati
-IP = env.str("ip")  # Xosting ip manzili
+# Reading datas from .env file
+# Bot
+BOT_TOKEN = env.str("BOT_TOKEN")  # BOT TOKEN
+ADMINS = env.list("ADMINS")  # admins list
+IP = env.str("ip")  # host api address
+
+# Postgresql
+DB_USER = env.str('DB_USER')
+DB_NAME = env.str('DB_NAME')
+DB_PASSWORD = env.str('DB_PASSWORD')
+DB_HOST = env.str('DB_HOST')
+DB_PORT = env.str('DB_PORT')
+
+# API KEYS
+CURR_WEATHER_API_KEY = env.str('CURR_WEATHER_API_KEY')
+SPORT_API_KEY = env.str('SPORT_API_KEY')
