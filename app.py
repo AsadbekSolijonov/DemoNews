9349f11306e33a03613179616f6b4b21f8e97b36
AsadbekSolijonov <iamsolijonovasadbek@gmail.com>
2023-04-09 14:47:56 +0500

    bot

--- app.py
+++ app.py
@@ -1,17 +1,32 @@
+import asyncio
+
 from aiogram import executor
 
-from loader import dp
+from handlers.users.sport_send_news import update_sport_news
+from loader import dp, db
 import middlewares, filters, handlers
 from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
+from handlers.users.weath_send_news import update_weather_news
 
 
 async def on_startup(dispatcher):
+    #  db connection
+    await db.connect_pool()
+    # drop table
+    await db.drop_table()
+    # create table users if not exists
+    await db.create_table_users()
     # Birlamchi komandalar (/star va /help)
     await set_default_commands(dispatcher)
 
     # Bot ishga tushgani haqida adminga xabar berish
     await on_startup_notify(dispatcher)
+
+    # update weather news
+    asyncio.ensure_future(update_weather_news())
+    # update sport news
+    asyncio.ensure_future(update_sport_news())
 
 
 if __name__ == '__main__':
