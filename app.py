1beac44a6183a21129eb116e7c068cd453daadb9
AsadbekSolijonov <iamsolijonovasadbek@gmail.com>
2023-04-10 09:19:25 +0500

    bot

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ import asyncio
 
 from aiogram import executor
 
-from handlers.users.sport_send_news import update_sport_news
+from handlers.users.sport import update_sport_news
 from loader import dp, db
 import middlewares, filters, handlers
 from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
-from handlers.users.weath_send_news import update_weather_news
+from handlers.users.weather import update_weather_news
 
 
 async def on_startup(dispatcher):
