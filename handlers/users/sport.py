7167800fbe0dbbda381ddfcec0095932231a205e
AsadbekSolijonov <iamsolijonovasadbek@gmail.com>
2023-04-16 02:22:30 +0500

    handler

--- handlers/users/sport.py
+++ handlers/users/sport.py
@@ -5,10 +5,10 @@ from loader import db, bot
 from api.sport_api import SportApiNews
 
 
-async def send_sport_news():
+async def send_sport_news(text):
     # (chat_id, weather_time) but we need not weather_time field
     datas = await db.chat_id_time_users()
-    text = SportApiNews().yield_text
+
     for user_id, _ in datas:
         # Text
         text = f'⛳️ Sport News\n\n{text.__next__()}'
@@ -18,9 +18,11 @@ async def send_sport_news():
 
 async def update_sport_news():
     # Infinite
+    # Text
+    text = SportApiNews().yield_text
     while True:
         # Running Function
-        await send_sport_news()
+        await send_sport_news(text)
         # Time
         datetime_ = datetime.datetime.now()
         min_ = datetime_.minute
